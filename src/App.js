28c3aac186d5d75241c2211413720ef5b628fbd4
Predrag <predragilijev@gmail.com>
2022-05-27 00:11:33 +0200

    Added form rating

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import { useState } from 'react'
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
+
 import FeedbackData from './data/feedbackData'
 
 function App() {
    const [feedback, setFeedback] = useState(FeedbackData)
+
+   const addFeedback = (newFeedback) => {
+      console.log(newFeedback)
+   }
 
    const deleteFeedback = (id) => {
       if(window.confirm('Are you sure you want to delete?')) {
@@ -17,6 +23,7 @@ function App() {
       <>
          <Header />
          <div className="container">
+            <FeedbackForm handleAdd={addFeedback} />
             <FeedbackStats feedback={feedback}/>
             <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
          </div>
